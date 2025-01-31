490a9da15bc416631ea881106c183a62ea621272
ConstroMan <constromanai@gmail.com>
2025-01-31 11:16:20 +0530

    Changes in logo file path

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export const Logo = () => (
   <div className="flex items-center">
     <div className="relative">
-      <img src="/Logo-removebg.png" alt="ConstroMan Logo" className="w-16 h-auto object-contain" />
+      <img src="public/images/Logo-removebg.png" alt="ConstroMan Logo" className="w-16 h-auto object-contain" />
     </div>
     <span className="text-xl font-strong tracking-wide">
       Constro<span className="font-medium text-teal-600">Man</span>
